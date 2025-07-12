4e1abd34631c8022555888a7836902fe470cae46
Vasek2024 <p.f1@yandex.ru>
2025-07-13 00:26:49 +0700

    Разбил шаблон на блоки

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -20,54 +20,12 @@ const Main = () => {
                 <ChoiceNav />
                 <section className="article__choice-sections">
                     <ChoiceSections />
-                    <div className="choice__sections">
-                        <h2 className="choice__sections-title">Сегодня</h2>
-                        <span className="choice__sections-month">06 июля</span>
-                        <img className="choice__sections-img" src="" alt=""/>
-                        <span className="choice__sections-min">+18°</span>
-                        <span className="choice__sections-max">+15°</span>
-                        <span className="choice__sections-sky">Облачно</span>
-                    </div>
-                    <div className="choice__sections">
-                        <h2 className="choice__sections-title">Сегодня</h2>
-                        <span className="choice__sections-month">06 июля</span>
-                        <img className="choice__sections-img" src="" alt=""/>
-                        <span className="choice__sections-min">+18°</span>
-                        <span className="choice__sections-max">+15°</span>
-                        <span className="choice__sections-sky">Облачно</span>
-                    </div>
-                    <div className="choice__sections">
-                        <h2 className="choice__sections-title">Сегодня</h2>
-                        <span className="choice__sections-month">06 июля</span>
-                        <img className="choice__sections-img" src="" alt=""/>
-                        <span className="choice__sections-min">+18°</span>
-                        <span className="choice__sections-max">+15°</span>
-                        <span className="choice__sections-sky">Облачно</span>
-                    </div>
-                    <div className="choice__sections">
-                        <h2 className="choice__sections-title">Сегодня</h2>
-                        <span className="choice__sections-month">06 июля</span>
-                        <img className="choice__sections-img" src="" alt=""/>
-                        <span className="choice__sections-min">+18°</span>
-                        <span className="choice__sections-max">+15°</span>
-                        <span className="choice__sections-sky">Облачно</span>
-                    </div>
-                    <div className="choice__sections">
-                        <h2 className="choice__sections-title">Сегодня</h2>
-                        <span className="choice__sections-month">06 июля</span>
-                        <img className="choice__sections-img" src="" alt=""/>
-                        <span className="choice__sections-min">+18°</span>
-                        <span className="choice__sections-max">+15°</span>
-                        <span className="choice__sections-sky">Облачно</span>
-                    </div>
-                    <div className="choice__sections">
-                        <h2 className="choice__sections-title">Сегодня</h2>
-                        <span className="choice__sections-month">06 июля</span>
-                        <img className="choice__sections-img" src="" alt=""/>
-                        <span className="choice__sections-min">+18°</span>
-                        <span className="choice__sections-max">+15°</span>
-                        <span className="choice__sections-sky">Облачно</span>
-                    </div>
+                    <ChoiceSections />
+                    <ChoiceSections />
+                    <ChoiceSections />
+                    <ChoiceSections />
+                    <ChoiceSections />
+                    <ChoiceSections />
                 </section>
             </article>
         </main>
