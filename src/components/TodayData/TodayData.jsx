8794620eaf3c6b22facb945a60f92798941d18ad
Vasek2024 <p.f1@yandex.ru>
2025-07-16 11:59:44 +0700

    Работа с выводом данных и времени

--- src/components/TodayData/TodayData.jsx
+++ src/components/TodayData/TodayData.jsx
@@ -1,30 +1,37 @@
 import './TodayData.css'
 
-const TodayData = () => {
+const TodayData = ({weather}) => {
     
     return (
-        <section className="article__today-data">
-            <div className="today__data-temp">
-                <img className="today__temp-img" src="./public/thermometer 1.svg" alt=""></img>
-                <span className="today__temp-temp">Температура</span>
-                <span className="today__temp-difference">20° - ощущается как 17°</span>
-            </div>
-            <div className="today__data-pressure">
-                <img className="today__pressure-img" src="./public/humidity 1.svg" alt=""></img>
-                <span className="today__pressure-pressure">Давление </span>
-                <span className="today__temp-ok">765 мм ртутного столба - нормальное</span>
-            </div>
-            <div className="today__data-precipitation">
-                <img className="today__precipitation-img" src="./public/Group (4).svg" alt=""></img>
-                <span className="today__precipitation-precipitation">Осадки</span>
-                <span className="today__temp-nou">Без осадков</span>
-            </div>
-            <div className="today__data-wind">
-                <img className="today__wind-img" src="./public/wind 1.svg" alt=""></img>
-                <span className="today__wind-wind">Ветер</span>
-                <span className="today__temp-direction">3 м/с юго-запад - легкий ветер</span>
-            </div>
-        </section>
+        weather && (
+            <section className="article__today-data">
+                <div className="today__data-temp">
+                    <img className="today__temp-img" src="./public/thermometer 1.svg" alt="Температура"></img>
+                    <span className="today__temp-temp">Температура</span>
+                    <span className="today__temp-difference">{weather.temperature}<sup>o</sup> - ощущается как {weather.feelsLike}<sup>o</sup></span>
+                </div>
+                <div className="today__data-pressure">
+                    <img className="today__pressure-img" src="./public/humidity 1.svg" alt="Давление"></img>
+                    <span className="today__pressure-pressure">Давление </span>
+                    <span className="today__temp-ok">{weather.pressure} мм ртутного столба</span>
+                </div>
+                <div className="today__data-precipitation">
+                    <img className="today__precipitation-img" src="./public/Group (4).svg" alt="Осадки"></img>
+                    <span className="today__precipitation-precipitation">Осадки</span>
+
+                    {/* СДЕЛАТЬ ПЕРЕВОД */}
+                    <span className="today__temp-nou">{weather.precipitation}</span>
+
+                </div>
+                <div className="today__data-wind">
+                    <img className="today__wind-img" src="./public/wind 1.svg" alt="Ветер"></img>
+                    <span className="today__wind-wind">Ветер</span>
+
+                    {/* СДЕЛАТЬ ПЕРЕВОД */}
+                    <span className="today__temp-direction">{weather.windSpeed} м/с {weather.description}</span>
+                </div>
+            </section>
+        )
     )
 }
 
--- src/components/TodayTemperature/TodayTemperature.jsx
+++ src/components/TodayTemperature/TodayTemperature.jsx
@@ -1,14 +1,22 @@
+// import React, { useState, useEffect } from "react";
+import { useState } from 'react'
 import './TodayTemperature.css'
 
-const TodayTemperature = () => {
+const TodayTemperature = ({weather}) => {
+
+  const [now, setNow] = useState(new Date())
+  setInterval(() => {setNow(new Date()), 1000})
+
 return (
-    <section className="article__today-temperature">
-        <span className="today-temperature__temp">20°</span>
-        <span className="today-temperature__day">Сегодня</span>
-        <span className="today-temperature__time">Время: 21:54</span>
-        <img className="today-temperature__icon" src="./public/Group (1).svg" alt=""/>
-        <span className="today-temperature__city">Город: Санкт-Петербург</span>
-    </section>
+        weather && (
+            <section className="article__today-temperature">
+              <span className="today-temperature__temp">{weather.temperature}<sup>o</sup></span>
+              <span className="today-temperature__day">Сегодня</span>
+              <span className="today-temperature__time">Время: {now.toLocaleTimeString()}</span>
+              <img className="today-temperature__icon" src={weather.icon} alt={weather.precipitation}/>
+              <span className="today-temperature__city">Город: {weather.location}</span>
+          </section>
+        )
 )
 }
 export default TodayTemperature
