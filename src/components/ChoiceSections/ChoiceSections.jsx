580761e336b9b57086c2a26769c71e4be27249fc
Vasek2024 <p.f1@yandex.ru>
2025-07-28 23:19:53 +0700

    продолжение

--- src/components/ChoiceSections/ChoiceSections.jsx
+++ src/components/ChoiceSections/ChoiceSections.jsx
@@ -4,14 +4,14 @@ const ChoiceSections = ({weatherFiveDays}) => {
     console.log(weatherFiveDays);
 
     return (
-        weatherFiveDays && weatherFiveDays.map(d =>(
+        weatherFiveDays && weatherFiveDays.map((d, index) =>(
         <div className="choice__sections">
-            <h2 className="choice__sections-title">Сегодня</h2>
+            {/* <h2 className="choice__sections-title">Сегодня</h2> */}
             <span className="choice__sections-month">06 июля</span>
-            <img className="choice__sections-img" src = {weatherFiveDays[2].weather[0].icon}/>
-            <span className="choice__sections-min">{Math.floor(weatherFiveDays[2].main.temp)}<sup>o</sup></span>
-            <span className="choice__sections-max">{Math.floor(weatherFiveDays[2].main.temp_max)}<sup>o</sup></span>
-            <span className="choice__sections-sky">{weatherFiveDays[2].weather[0].description}</span>
+            <img className="choice__sections-img" src = {weatherFiveDays[index].weather[0].icon}/>
+            <span className="choice__sections-min">{Math.floor(weatherFiveDays[index].main.temp)}<sup>o</sup></span>
+            <span className="choice__sections-max">{Math.floor(weatherFiveDays[index].main.temp_max)}<sup>o</sup></span>
+            <span className="choice__sections-sky">{weatherFiveDays[index].weather[0].description}</span>
         </div>
         )
     ))
